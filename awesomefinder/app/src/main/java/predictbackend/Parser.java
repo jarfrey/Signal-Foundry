9af6593c6f250d0cc19eaf6f308b5f4b2af9107c
Kiwi-the-Cat <remiozzy@gmail.com>
2026-09-19 16:58:53 -0400

    get int or string from field in given JSON block with getString() and getInt()

--- awesomefinder/app/src/main/java/predictbackend/Parser.java
+++ awesomefinder/app/src/main/java/predictbackend/Parser.java
@@ -19,36 +19,47 @@ public class Parser {
         Path name = Path.of(filename);
         // read the file to a string
         str = Files.readString(name);
-
+        // turn the string into a JSONObject for parsing
         json = new JSONObject(str);
-
+        // get the array of job entries
         jobs = json.getJSONArray("jobs");
     }
 
+    // returns arraylist of JSONObjects
     public ArrayList<JSONObject> split_JSONObject() {
-
+        // convert from JSONArray to ArrayList
         ArrayList<JSONObject> split_array = new ArrayList<>(jobs.length());
         for (int i = 0; i < jobs.length(); i++) {
             split_array.add(jobs.getJSONObject(i));
         }
-
-        for (JSONObject obj : split_array) {
-            String str = obj.toString();
-            System.out.println(str);
-        }
-
         return split_array;
     }
 
+    // return arraylist of strings
     public ArrayList<String> split_String() {
-
+        // convert from jsonobject to string
         ArrayList<String> split_array = new ArrayList<>(jobs.length());
         for (int i = 0; i < jobs.length(); i++) {
             split_array.add(jobs.getJSONObject(i).toString());
-            System.out.println(split_array.get(i));
         }
-
         return split_array;
+    }
+
+    // pass key and block
+    // returns the value of the requested key as a string
+    public String getString(String key, String jsonBlock) {
+        // convert given block to usable json object
+        JSONObject block = new JSONObject(jsonBlock);
+
+        return block.get(key).toString();
+    }
+    
+    // pass key and block
+    // does the same thing as getString() but int
+    public int getInt(String key, String jsonBlock) {
+        JSONObject block = new JSONObject(jsonBlock);
+
+        return block.getInt(key);
     }
 }
 
